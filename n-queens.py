fbb1fb63568ad7bdf3ee59e2b23c75741941402c
kellyfreet13 <kelly.freet13@yahoo.com>
2019-02-14 11:26:31 -0800

    made a class:

--- n-queens.py
+++ n-queens.py
@@ -1,5 +1,10 @@
 import sys
 
+class NQueens:
+
+    def selection(self):
+        print('a')
+
 # in a cmd prompt, type (without < >) <python n-queens.py some args>
 if __name__ == "__main__":
     print(sys.argv)
