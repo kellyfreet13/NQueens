154a5e0a9ebdee8635eb443920cb4e30221273a4
Khoa100 <minhkhoavu954@gmail.com>
2019-02-14 11:16:13 -0800

    Update n-queens.py

--- n-queens.py
+++ n-queens.py
@@ -1,10 +1,5 @@
 import sys
 
-import numpy as np
-class Board:
-    def __init__(self,numStates):
-        self.states = [['- ' for i in range(numStates)] for j in range(numStates)]
-        self.states = np.matrix(self.states)
 # in a cmd prompt, type (without < >) <python n-queens.py some args>
 if __name__ == "__main__":
     print(sys.argv)
