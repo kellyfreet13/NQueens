558035da793c1a11f96a11bcee178331fcbf2d81
kellyfreet13 <kelly.freet13@yahoo.com>
2019-02-14 23:03:22 -0800

    corrected use of state to use of num_queens for inital board generation, and one queen per row intitally

--- board.py
+++ board.py
@@ -6,7 +6,15 @@ Created on Thu Feb 14 11:15:50 2019
 """
 
 import numpy as np
+import random
+
+
 class Board:
-    def __init__(self,numStates):
-        self.states = [['- ' for i in range(numStates)] for j in range(numStates)]
-        self.states = np.matrix(self.states)
+    # generate one queen per row, represented as a 1 for Queen
+    def __init__(self, num_queens):
+        self.board = []
+        for i in range(num_queens):
+            row = np.zeros(num_queens)
+            rand = random.randint(0, num_queens-1)  # rand is inclusive
+            row[rand] = 1
+            self.board.append(row)
